7737fd38edfa44993629b7f21a08e8e64d8b3817
NAVEEN DAS <dasnaveen230@gmail.com>
2025-03-28 02:44:51 +0530

    database

--- config/db.js
+++ config/db.js
@@ -1,17 +1,11 @@
-const mongoose = require("mongoose");
-require("dotenv").config();
+const mongoose = require('mongoose');
 
-const connectDB = async () => {
-    try {
-        await mongoose.connect(process.env.MONGO_URI, {
-            useNewUrlParser: true,
-            useUnifiedTopology: true
-        });
-        console.log("MongoDB Connected...");
-    } catch (error) {
-        console.error(error.message);
-        process.exit(1);
-    }
-};
-
-module.exports = connectDB;
+mongoose.connect('mongodb+srv://rupeshdash20:2W4LNi93lZk93mBd@cluster0.gt8be.mongodb.net/', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true
+}).then(() => {
+  console.log('Connected to MongoDB');
+}).catch((err) => {
+  console.error('Error connecting to MongoDB', err);
+});
+    
