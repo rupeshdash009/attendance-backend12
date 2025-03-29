b1f419092b38186d36f9fd1dba83c883456bbb20
Rupesh Dash <rupeshdash20@gmail.com>
2025-03-30 00:04:37 +0530

    -

--- index.js
+++ index.js
@@ -3,12 +3,17 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 console.log('ENV:', process.env); 
+const cors = require("cors");
+
 
 // Middleware
 app.use(express.json());
 
 // MongoDB Connection
 const PORT = process.env.PORT || 5000;
+app.listen(PORT, "0.0.0.0", () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 // Add connection error handling
 if (!process.env.MONGO_URI) {
@@ -27,3 +32,12 @@ mongoose.connect(process.env.MONGO_URI)
     console.error("❌ MongoDB Connection Error:", error.message);
     process.exit(1);
   });
+
+  // Allow requests from your frontend (Vercel domain)
+app.use(
+  cors({
+      origin: "https://attendance-portal01.vercel.app/",  // Replace with your frontend URL
+      methods: "GET,POST,PUT,DELETE",
+      credentials: true
+  })
+);
